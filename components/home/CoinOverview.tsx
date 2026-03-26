cc61174bf65a331c0a539314c06d8e20f12665ba
pankajyadav2006 <pankaj.yadav@adypu.edu.in>
2026-03-26 13:58:33 +0530

    feat: implement coin overview with interactive candlestick chart

---
+++ components/home/CoinOverview.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Image from 'next/image';
+import { formatCurrency } from '@/lib/utils';
+import { CoinOverviewFallback } from './fallback';
+import CandlestickChart from '@/components/CandlestickChart';
+import { fetcher } from '@/lib/coingecko.actions';
+const CoinOverview = async () => {
+  try {
+    const [coin, coinOHLCData] = await Promise.all([
+      fetcher<CoinDetailsData>('/coins/bitcoin', {
+        dex_pair_format: 'symbol',
+      }),
+      fetcher<OHLCData[]>('/coins/bitcoin/ohlc', {
+        vs_currency: 'inr',
+        days: 1,
+        precision: 'full',
+      }),
+    ]);
+
+    return (
+      <div id="coin-overview">
+        <CandlestickChart data={coinOHLCData} coinId="bitcoin">
+          <div className="header pt-2">
+            <Image src={coin.image?.large} alt={coin.name} width={56} height={56} />
+            <div className="info">
+              <p>
+                {coin.name} / {coin.symbol.toUpperCase()}
+              </p>
+              <h1>{formatCurrency(coin.market_data.current_price.inr)}</h1>
+            </div>
+          </div>
+        </CandlestickChart>
+      </div>
+    );
+  } catch (error) {
+    console.error('Error fetching coin overview:', error);
+    return <CoinOverviewFallback />;
+  }
+};
+
+export default CoinOverview;

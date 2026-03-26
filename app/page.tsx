50af597188cb488a4696d6cebafa4b12747ccc93
pankajyadav2006 <pankaj.yadav@adypu.edu.in>
2026-03-26 13:58:33 +0530

    feat: implement homepage entry point and main layout sections

---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+import {
+  CoinOverviewFallback,
+  TrendingCoinsFallback,
+  CategoriesFallback,
+} from '@/components/home/fallback'
+import { Suspense } from 'react'
+import Categories from '@/components/Categories'
+import CoinOverview from '@/components/home/CoinOverview'
+import TrendingCoins from '@/components/home/TrendingCoins'
+const page = async () => {
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+      <section className="w-full mt-7 space-y-4">
+        <Suspense fallback={<CategoriesFallback />}>
+          <Categories />
+        </Suspense>
+      </section>
+    </main>
+  )
+}
+
+export default page
